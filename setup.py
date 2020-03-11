df6c73570fd0d8939011225e64b17988f41a0e2a
Hiroki Hata <hataidryv@gmail.com>
2020-03-11 10:57:01 +0900

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='sphinx_smx_theme',
-    version='1.0.35',
+    version='1.0.36',
     author='hata',
     author_email='hata@systemmetrix.jp',
     url='www.systemmetrix.jp',
