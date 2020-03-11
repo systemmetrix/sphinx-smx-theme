c12a8f09ab79e87fe77abd537958ba43cef69e17
Hiroki Hata <hataidryv@gmail.com>
2020-03-11 10:12:12 +0900

    update

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+from setuptools import find_packages
+
 
 setup(
     name='sphinx_smx_theme',
-    version='1.0.0',
+    version='1.0.35',
+    author='sakaguchi',
+    author_email='sakaguchi@systemmetrix.jp',
+    url='www.systemmetrix.jp',
+    zip_safe=False,
+    include_package_data=True,
     packages=find_packages(),
-
-    author='Hiroki Hata',
-    author_email='hata@systemmetrix.jp',
-
-    package_data={'sphinx_smx_theme': [
-        '*',
-        'static/*',
-    ]},
+    install_requires=[],
+    tests_require=[],
+    setup_requires=[],
+    entry_points={
+        'sphinx.html_themes': [
+            'sphinx_smx_theme = sphinx_smx_theme',
+        ]
+    },
 )
